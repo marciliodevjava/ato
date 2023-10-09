195855c96f7f067d86eb9ed9eddd6277469bb1ab
marciliodevjava <marciliodevjava@gmail.com>
2023-10-09 19:49:25 -0300

    Create AtoaApplication

---
+++ src/main/java/br/com/atoa/atoa/AtoaApplication.java
@@ -0,0 +1,13 @@
+package br.com.atoa.atoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtoaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtoaApplication.class, args);
+    }
+
+}
