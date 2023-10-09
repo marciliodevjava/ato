7899915f496b1956ae4dfe4dcf0cb128a1aaa1c3
marciliodevjava <marciliodevjava@gmail.com>
2023-10-09 19:45:12 -0300

    Create PessoaService

---
+++ src/main/java/br/com/atoa/atoa/service/PessoaService.java
@@ -0,0 +1,87 @@
+package br.com.atoa.atoa.service;
+
+import br.com.atoa.atoa.domain.Endereco;
+import br.com.atoa.atoa.domain.Pessoa;
+import br.com.atoa.atoa.dto.request.PessoaDto;
+import br.com.atoa.atoa.dto.response.DadosDtoResponse;
+import br.com.atoa.atoa.dto.response.EnderecoDtoResponse;
+import br.com.atoa.atoa.dto.response.PessoaDtoRespose;
+import br.com.atoa.atoa.mapper.PessoaMapper;
+import br.com.atoa.atoa.repository.EnderecoRepository;
+import br.com.atoa.atoa.repository.PessoaRepositry;
+import br.com.atoa.atoa.utils.PesquisaEnderecoUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
+public class PessoaService {
+    @Autowired
+    private PessoaMapper pessoaMapper;
+    @Autowired
+    private PessoaRepositry pessoaRepositry;
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+    @Autowired
+    private PesquisaEnderecoUtils pesquisaEndereco;
+    public DadosDtoResponse salvarPessoa(PessoaDto dto) {
+
+        Endereco endereco = pesquisaEndereco.pesquisarCep(dto.getCep());
+        Pessoa pessoa = pessoaMapper.mapearPessoaDtoParaPessoa(dto);
+        if (Objects.nonNull(endereco)){
+            pessoa.setEndereco(endereco);
+            endereco.setPessoa(pessoa);
+        }
+        if (Objects.nonNull(pessoa)) pessoa = pessoaRepositry.save(pessoa);
+        if (Objects.nonNull(endereco)) endereco = enderecoRepository.save(endereco);
+        DadosDtoResponse dados = this.montarDadosRetorno(pessoa);
+        return dados;
+    }
+
+    private DadosDtoResponse montarDadosRetorno(Pessoa pessoa) {
+        DadosDtoResponse dados = new DadosDtoResponse();
+        if (Objects.nonNull(pessoa)){
+            PessoaDtoRespose dto = new PessoaDtoRespose();
+            dto.setId(pessoa.getId());
+            dto.setNome(pessoa.getNome());
+            dto.setCpf(pessoa.getCpf());
+            dto.setRg(pessoa.getRg());
+            dados.setPessoa(dto);
+            dados.getPessoa().setEndereco(this.mapeiaEndereco(pessoa.getEndereco()));
+            return dados;
+        }
+        return dados;
+    }
+
+    private EnderecoDtoResponse mapeiaEndereco(Endereco endereco) {
+        if (Objects.nonNull(endereco)){
+            EnderecoDtoResponse dto = new EnderecoDtoResponse();
+            dto.setCep(endereco.getCep());
+            dto.setUf(endereco.getUf());
+            dto.setDdd(endereco.getDdd());
+            dto.setBairro(endereco.getBairro());
+            dto.setLocalidade(endereco.getLocalidade());
+            dto.setComplemento(endereco.getComplemento());
+            dto.setLogradouro(endereco.getLogradouro());
+            return dto;
+        }
+        return null;
+    }
+
+    public DadosDtoResponse buscarPessoa(Long id) {
+        DadosDtoResponse dto = new DadosDtoResponse();
+        if (id != null){
+            Optional<Pessoa> pessoa = pessoaRepositry.findById(id);
+            dto = this.montarDadosRetorno(this.conversePessoa(pessoa));
+            return dto;
+        }
+        return dto;
+    }
+
+    private Pessoa conversePessoa(Optional<Pessoa> pessoa) {
+        Pessoa ps = pessoa.get();
+        return ps;
+    }
+}
