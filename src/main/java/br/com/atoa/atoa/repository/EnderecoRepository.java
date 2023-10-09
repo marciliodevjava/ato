0c641b529e93a5936ad11e1cfefb38d0dc843625
marciliodevjava <marciliodevjava@gmail.com>
2023-10-09 19:47:42 -0300

    Create EnderecoRepository

---
+++ src/main/java/br/com/atoa/atoa/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package br.com.atoa.atoa.repository;
+
+import br.com.atoa.atoa.domain.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
