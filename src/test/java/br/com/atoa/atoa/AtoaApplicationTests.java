778c9f069785bb6fe9aeee2000b38c85c447a714
marciliodevjava <marciliodevjava@gmail.com>
2023-10-09 19:49:19 -0300

    Create AtoaApplication

---
+++ src/test/java/br/com/atoa/atoa/AtoaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.atoa.atoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtoaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
