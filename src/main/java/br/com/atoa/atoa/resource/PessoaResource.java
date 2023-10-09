ad777e0691418b05739035eb9204fc98a0a57869
marciliodevjava <marciliodevjava@gmail.com>
2023-10-09 19:45:33 -0300

    Create PessoaResource

---
+++ src/main/java/br/com/atoa/atoa/resource/PessoaResource.java
@@ -0,0 +1,31 @@
+package br.com.atoa.atoa.resource;
+
+import br.com.atoa.atoa.dto.request.PessoaDto;
+import br.com.atoa.atoa.dto.response.DadosDtoResponse;
+import br.com.atoa.atoa.service.PessoaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/pessoa")
+public class PessoaResource {
+    @Autowired
+    private PessoaService pessoaService;
+
+    @PostMapping
+    public ResponseEntity<DadosDtoResponse> salvarPessoa(@RequestBody PessoaDto dto, UriComponentsBuilder builder ){
+        DadosDtoResponse dados = pessoaService.salvarPessoa(dto);
+        URI uri = builder.path("/pessoa/{id}").buildAndExpand(dados.getPessoa().getId()).toUri();
+        return ResponseEntity.created(uri).body(dados);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DadosDtoResponse> buscarPessoa(@PathVariable Long id){
+        DadosDtoResponse dto = pessoaService.buscarPessoa(id);
+        return ResponseEntity.ok(dto);
+    }
+}
