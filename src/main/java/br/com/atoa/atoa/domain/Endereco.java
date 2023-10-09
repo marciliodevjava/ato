6959c5344287f8c8782e5a65113a1fd50e986304
marciliodevjava <marciliodevjava@gmail.com>
2023-10-09 19:48:56 -0300

    Create Endereco

---
+++ src/main/java/br/com/atoa/atoa/domain/Endereco.java
@@ -0,0 +1,44 @@
+package br.com.atoa.atoa.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "td_endereco")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Endereco implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 2L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @JsonProperty("cep")
+    private String cep;
+    @JsonProperty("logradouro")
+    private String logradouro;
+    @JsonProperty("complemento")
+    private String complemento;
+    @JsonProperty("bairro")
+    private String bairro;
+    @JsonProperty("localidade")
+    private String localidade;
+    @JsonProperty("uf")
+    private String uf;
+    @JsonProperty("ddd")
+    private String ddd;
+    @OneToOne
+    @JoinColumn(name = "id_pessoa")
+    private Pessoa pessoa;
+}
