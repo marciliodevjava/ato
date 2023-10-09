1d0082bb3592db0b2a5408c0313aa80e87eb5f7c
marciliodevjava <marciliodevjava@gmail.com>
2023-10-09 19:48:50 -0300

    Create EnderecoDtoResponse

---
+++ src/main/java/br/com/atoa/atoa/dto/response/EnderecoDtoResponse.java
@@ -0,0 +1,18 @@
+package br.com.atoa.atoa.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class EnderecoDtoResponse {
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String ddd;
+}
