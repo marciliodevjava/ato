1b6e4bd12c2df6f5a541ab8b1f8e8fb72a40c0f3
marciliodevjava <marciliodevjava@gmail.com>
2023-10-10 21:11:27 -0300

    Update PessoaResource

--- src/main/java/br/com/atoa/atoa/resource/PessoaResource.java
+++ src/main/java/br/com/atoa/atoa/resource/PessoaResource.java
@@ -2,6 +2,7 @@ package br.com.atoa.atoa.resource;
 
 import br.com.atoa.atoa.dto.request.PessoaDto;
 import br.com.atoa.atoa.dto.response.DadosDtoResponse;
+import br.com.atoa.atoa.dto.response.ListDadosDtoResponse;
 import br.com.atoa.atoa.service.PessoaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/pessoa")
@@ -17,15 +19,21 @@ public class PessoaResource {
     private PessoaService pessoaService;
 
     @PostMapping
-    public ResponseEntity<DadosDtoResponse> salvarPessoa(@RequestBody PessoaDto dto, UriComponentsBuilder builder ){
+    public ResponseEntity<DadosDtoResponse> salvarPessoa(@RequestBody PessoaDto dto, UriComponentsBuilder builder) {
         DadosDtoResponse dados = pessoaService.salvarPessoa(dto);
         URI uri = builder.path("/pessoa/{id}").buildAndExpand(dados.getPessoa().getId()).toUri();
         return ResponseEntity.created(uri).body(dados);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<DadosDtoResponse> buscarPessoa(@PathVariable Long id){
+    public ResponseEntity<DadosDtoResponse> buscarPessoa(@PathVariable Long id) {
         DadosDtoResponse dto = pessoaService.buscarPessoa(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping("/buscar/{nome}")
+    public ResponseEntity<ListDadosDtoResponse> buscarAllPessoa(@PathVariable String nome) {
+        ListDadosDtoResponse dto = pessoaService.buscarListaPessoa(nome);
         return ResponseEntity.ok(dto);
     }
 }
