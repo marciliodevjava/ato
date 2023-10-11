e8a285e4b83d1474f9f1ee70d5a0c3307ff4d7ee
marciliodevjava <marciliodevjava@gmail.com>
2023-10-10 21:11:18 -0300

    Update PessoaRepositry

--- src/main/java/br/com/atoa/atoa/repository/PessoaRepositry.java
+++ src/main/java/br/com/atoa/atoa/repository/PessoaRepositry.java
@@ -4,6 +4,9 @@ import br.com.atoa.atoa.domain.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PessoaRepositry extends JpaRepository<Pessoa, Long> {
+    List<Pessoa> findAllByNome(String nome);
 }
